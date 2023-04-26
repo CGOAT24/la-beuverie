f41c129c0820caa284b3051ecda1fd5f5da7a273
Chad Gauthier <chad.gauthier32@gmail.com>
2023-04-25 21:45:27 -0400

    updated client & readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-TODO
+# Start the app
+
+```bash
+docker compose build --pull .   # builds the latest version
+docker compose up -d            # start the app
+
+```
--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,30 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import data from './assets/data.json';
+import Card from "./components/card.vue";
+
+const drinks = data.drinks;
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="grid">
+    <card v-for="drink in drinks" :drink="drink" class="card"/>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+#grid {
+  background-color: #084227;
+  display: inline-grid;
+  justify-content: space-around;
+  align-content: space-around;
+  grid-template-columns: auto auto auto;
+  border-radius: 15px;
+  width: 100%;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.card {
+  height: auto;
+  aspect-ratio: 2;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
