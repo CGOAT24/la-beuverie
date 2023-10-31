dc429d0c2cdea6acf6b34ca049862836e60a53a7
Chad Gauthier <chad.gauthier32@gmail.com>
2023-10-31 16:31:59 -0400

    update index page

--- client/components/Card.vue
+++ client/components/Card.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
+
+const props = defineProps({
+    name: String,
+    identifier: String
+});
 </script>
 <template>
-    <div class="w-full h-full p-4 my-2.5 bg-[#EC8091] rounded-2xl border-4">
-        <div class="text-center">Margharita</div>
+    <div class="p-4 m-4 bg-[#FFF3E7] rounded-2xl border-4 border-[#12110F] shadow-[5px_5px_#12110F]">
+        <div class="text-center font-bold uppercase">{{ props.name }}</div>
     </div>
 </template>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts">
+import {drinks} from "../assets/data";
+import { MagnifyingGlassIcon, PlusIcon, FunnelIcon } from '@heroicons/vue/24/outline';
 </script>
 <template>
-    <Card class="aspect-2" />
+    <div>
+        <div class="w-100 bg-[#FFF3E7] py-4 rounded-2xl border-4 mx-3 mt-1 border-[#12110F] shadow-[5px_5px_#12110F] flex flex-wrap justify-end">
+            <button class="border-2 border-[#12110F] rounded-xl py-1 px-3 mx-1 bg-[#A5EFFF]"><FunnelIcon class="h-6 w-6 bg-[#A5EFFF]"/></button>
+            <button class="border-2 border-[#12110F] rounded-xl py-1 px-3 mx-1 bg-[#FDF0AB]"><PlusIcon class="h-6 w-6 bg-[#FDF0AB]"/></button>
+            <button class="border-2 border-[#12110F] rounded-xl py-1 px-3 mx-1 bg-[#FEA9AA]"><MagnifyingGlassIcon class="h-6 w-6 bg-[#FEA9AA]"/></button>
+        </div>
+        <div class="mt-3 flex flex-wrap justify-start">
+            <Card :name="drink.name" v-for="drink in drinks" v-bind:key="drink.name"/>
+        </div>
+    </div>
 </template>
