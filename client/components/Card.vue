f596caa28941de889f4c17b476b8d04edbb19448
Chad Gauthier <chad.gauthier32@gmail.com>
2023-10-31 22:26:19 -0400

    added eslint

--- client/components/Card.vue
+++ client/components/Card.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
 
 const props = defineProps({
-    name: String,
-    identifier: String
+    name: {
+        type: String,
+        required: true
+    },
+    identifier: {
+        type: String,
+        required: true
+    }
 });
 </script>
 <template>
-    <button class="p-4 m-4 bg-[#FFF3E7] hover:bg-[#C5A0FF] rounded-2xl border-4 border-[#12110F] shadow-[5px_5px_#12110F] text-center font-bold uppercase card-button">{{ props.name }}</button>
+  <button class="p-4 m-4 bg-[#FFF3E7] hover:bg-[#C5A0FF] rounded-2xl border-4 border-[#12110F] shadow-[5px_5px_#12110F] text-center font-bold uppercase card-button">
+    {{ props.name }}
+  </button>
 </template>
