712e2f08a8dc93e37a329462389ab893d59db2a3
Chad Gauthier <chad.gauthier32@gmail.com>
2024-06-22 22:16:43 -0400

    css cleanup

--- src/components/inputs/InputTags.svelte
+++ src/components/inputs/InputTags.svelte
@@ -1,10 +1,6 @@
 <script lang="ts">
-	import { createEventDispatcher } from 'svelte';
-
 	export let selectedOptions: string[] = [];
 	let chipInput = '';
-
-	createEventDispatcher();
 
 	const addChip = () => {
 		if (chipInput.trim() !== '') {
@@ -17,13 +13,13 @@
 		selectedOptions = selectedOptions.filter((_, i) => i !== index);
 	};
 
-	const handleKeyPress = (event) => {
+	const handleKeyPress = (event: KeyboardEvent) => {
 		if (event.key === 'Enter') {
 			addChip();
 		}
 	};
 
-	const handleInput = (event) => {
+	const handleInput = (event: { target: { value: string } }) => {
 		chipInput = event.target.value;
 	};
 
@@ -33,10 +29,14 @@
 </script>
 
 <div
-	class="flex flex-wrap rounded-md border-2 border-black p-[10px] font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] outline-none"
+	class="flex flex-wrap rounded-md border-2 border-black p-[10px] font-bold shadow-medium outline-none"
 >
 	{#each selectedOptions as chip, index}
-		<button type="button" class="chip" on:click={() => removeChip(index)}>
+		<button
+			type="button"
+			class="m-2 py-1 px-2 bg-[#87CEEB] rounded cursor-pointer"
+			on:click={() => removeChip(index)}
+		>
 			{chip}
 		</button>
 	{/each}
@@ -46,19 +46,8 @@
 		on:keypress={handleKeyPress}
 		on:input={handleInput}
 		on:blur={handleBlur}
-		placeholder="Enter a tag"
+		placeholder={selectedOptions.length === 0 ? 'Enter a tag' : ''}
 		class="outline-none"
 		name="tags"
 	/>
 </div>
-
-<style>
-	.chip {
-		margin: 0.5rem;
-		padding: 0.25rem 0.5rem;
-		background-color: #007bff;
-		color: white;
-		border-radius: 0.25rem;
-		cursor: pointer;
-	}
-</style>
