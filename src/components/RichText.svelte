8e67694ae5fde9eaa286be898019e7ec6992a8d1
Chad Gauthier <57163675+CGOAT24@users.noreply.github.com>
2024-06-22 23:33:54 -0400

    view drink page (#254)

---
+++ src/components/RichText.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { Editor } from '@tiptap/core';
+	import { onMount } from 'svelte';
+	import { StarterKit } from '@tiptap/starter-kit';
+
+	export let value: string = '';
+
+	let element: Element;
+
+	onMount(() => {
+		new Editor({
+			editorProps: {
+				attributes: {
+					class: 'focus:outline-none'
+				}
+			},
+			element: element,
+			extensions: [StarterKit],
+			content: value,
+			editable: false
+		});
+	});
+</script>
+
+<div class="border-black shadow-large border-2 rounded w-full ml-5">
+	<div bind:this={element} class="m-1" />
+</div>
--- src/routes/drinks/[id]/+page.svelte
+++ src/routes/drinks/[id]/+page.svelte
@@ -2,29 +2,32 @@
 	import type { PageServerLoad } from '../../../../.svelte-kit/types/src/routes/drinks/[id]/$types';
 	import Row from '../../../components/Row.svelte';
 	import Pill from '../../../components/Pill.svelte';
+	import RichText from '../../../components/RichText.svelte';
 
 	export let data: PageServerLoad;
 </script>
 
-<div class="flex flex-wrap w-full">
-	<Row>
-		<h1 class="font-extrabold text-8xl italic">{data.drink.name}</h1>
-	</Row>
-	<Row>
-		<ul>
-			{#each data.drink.tags as tag}
-				<li><Pill text={tag} /></li>
-			{/each}
-		</ul>
-	</Row>
-	<Row>
-		<table class="border-black border-2 w-full h-full px-5">
-			{#each data.drink.ingredients as ingredient}
-				<tr class="border-2">
-					<td class="w-full p-2">{ingredient}</td>
-				</tr>
-			{/each}
-		</table>
-		<p class="w-full px-5 h-full">{data.drink.directions}</p>
-	</Row>
+<div class="flex justify-center">
+	<div class="w-2/3">
+		<Row>
+			<h1 class="font-extrabold text-8xl italic text-left w-full">{data.drink.name}</h1>
+		</Row>
+		<Row>
+			<ul class="flex flex-wrap justify-start w-full">
+				{#each data.drink.tags as tag}
+					<li><Pill text={tag} /></li>
+				{/each}
+			</ul>
+		</Row>
+		<Row>
+			<table class="border-black border-2 h-full px-5 w-full mr-5">
+				{#each data.drink.ingredients as ingredient}
+					<tr class="border-2">
+						<td class="w-full p-2">{ingredient}</td>
+					</tr>
+				{/each}
+			</table>
+			<RichText bind:value={data.drink.directions} />
+		</Row>
+	</div>
 </div>
