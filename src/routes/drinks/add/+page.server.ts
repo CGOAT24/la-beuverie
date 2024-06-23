7eb6a92b2acb055e4ff99ce493ca190996f29bf5
Chad Gauthier <chad.gauthier32@gmail.com>
2024-06-22 22:16:53 -0400

    add drink

--- src/routes/drinks/add/+page.server.ts
+++ src/routes/drinks/add/+page.server.ts
@@ -1,29 +1,8 @@
-import type { Actions, PageServerLoad } from './$types';
-import { fail, redirect } from '@sveltejs/kit';
-import { type CreateDrinkRequest, validate } from '$lib/validations/createDrinkValidator';
-import { parse } from '$lib/server/formDataParser';
-import { drinkService } from '$lib/server/services/drinkService';
+import type { PageServerLoad } from './$types';
+import { redirect } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async (event) => {
 	if (!event.locals.user) {
 		redirect(302, '/auth/login');
 	}
-	return {
-		username: event.locals.user.username
-	};
 };
-
-export const actions = {
-	default: async ({ request }) => {
-		const drink = parse<CreateDrinkRequest>(await request.formData());
-
-		const { valid, errors } = validate(drink);
-		if (!valid) {
-			return fail(400, errors);
-		}
-
-		await drinkService.create(drink);
-
-		throw redirect(303, '/');
-	}
-} satisfies Actions;
