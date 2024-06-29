f457d3e2e3d9be8e917c7345fd54a22c1db90782
Chad Gauthier <chad.gauthier32@gmail.com>
2024-06-28 22:25:45 -0400

    update drink form

--- src/routes/drinks/[id]/+page.svelte
+++ src/routes/drinks/[id]/+page.svelte
@@ -3,31 +3,42 @@
 	import Row from '../../../components/Row.svelte';
 	import Pill from '../../../components/Pill.svelte';
 	import RichText from '../../../components/RichText.svelte';
+	import Icon from '@iconify/svelte';
 
 	export let data: PageServerLoad;
+	const { isAuthor, drink } = data;
 </script>
 
 <div class="flex justify-center">
 	<div class="w-2/3">
 		<Row>
-			<h1 class="font-extrabold text-8xl italic text-left w-full">{data.drink.name}</h1>
+			<div class="flex justify-between w-full border-b-4 border-black">
+				<h1 class="font-extrabold text-8xl italic text-left w-full">
+					{drink.name}
+				</h1>
+				{#if isAuthor}
+					<a class="content-center justify-center" href="/drinks/{drink.id}/edit">
+						<Icon icon="heroicons:pencil-square" class="h-10 w-10 bg-inherit" />
+					</a>
+				{/if}
+			</div>
 		</Row>
 		<Row>
 			<ul class="flex flex-wrap justify-start w-full">
-				{#each data.drink.tags as tag}
+				{#each drink.tags as tag}
 					<li><Pill text={tag} /></li>
 				{/each}
 			</ul>
 		</Row>
 		<Row>
 			<table class="border-black border-2 h-full px-5 w-full mr-5">
-				{#each data.drink.ingredients as ingredient}
+				{#each drink.ingredients as ingredient}
 					<tr class="border-2">
 						<td class="w-full p-2">{ingredient}</td>
 					</tr>
 				{/each}
 			</table>
-			<RichText bind:value={data.drink.directions} />
+			<RichText bind:value={drink.directions} />
 		</Row>
 	</div>
 </div>
