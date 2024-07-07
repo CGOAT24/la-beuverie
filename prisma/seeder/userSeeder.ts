febbe603e1ac3c083a6e847e13d69fa0e1b32d19
Chad Gauthier <chad.gauthier32@gmail.com>
2024-07-07 00:49:29 -0400

    patch: fix seeder

--- prisma/seeder/userSeeder.ts
+++ prisma/seeder/userSeeder.ts
@@ -1,9 +1,18 @@
-import data from './data/users.json';
+import users from './data/users.json';
 import { Seeder } from './seeder';
+import { hashPassword } from '../../src/lib/server/hash';
 
 export class UserSeeder extends Seeder {
 	async up(): Promise<void> {
 		console.log('\x1b[44m%s\x1b[0m', 'Seeding users');
+
+		const data = await Promise.all(
+			users.map(async (x) => {
+				x.password = await hashPassword(x.password);
+				return x;
+			})
+		);
+
 		await this.prisma.user.createMany({
 			data: data,
 			skipDuplicates: true
