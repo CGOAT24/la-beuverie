da16345c6adde2e38bd50260e3f14a399a834e5e
Chad Gauthier <57163675+CGOAT24@users.noreply.github.com>
2024-07-07 20:02:56 -0400

    minor: fix input tags component (#272)

--- src/routes/drinks/add/+page.server.ts
+++ src/routes/drinks/add/+page.server.ts
@@ -1,8 +1,11 @@
 import type { PageServerLoad } from './$types';
 import { redirect } from '@sveltejs/kit';
+import { drinkService } from '$lib/server/services/drinkService';
 
 export const load: PageServerLoad = async (event) => {
+	const tags = await drinkService.getTags();
 	if (!event.locals.user) {
 		redirect(302, '/auth/login');
 	}
+	return { tags };
 };
