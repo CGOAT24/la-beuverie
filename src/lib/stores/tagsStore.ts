d88ccc472d911d97f1b19f0928d21aae48faf83c
Chad Gauthier <chad.gauthier32@gmail.com>
2024-07-09 19:18:49 -0400

    patch: fix api routes

--- src/lib/stores/tagsStore.ts
+++ src/lib/stores/tagsStore.ts
@@ -2,7 +2,12 @@ import { writable } from 'svelte/store';
 import { request } from '$lib/utils/HTTPRequest';
 
 const createStore = async () => {
-	const { data } = await request.get(`http://localhost:5173/api/tags`);
+	const baseUrl =
+		process.env.NODE_ENV === 'production'
+			? 'https://la-beuverie.vercel.app'
+			: 'http://localhost:5173';
+
+	const { data } = await request.get(`${baseUrl}/api/tags`);
 	return writable<string[]>(data);
 };
 
